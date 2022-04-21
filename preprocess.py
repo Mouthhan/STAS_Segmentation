2eeb3740913611861dddbd25d82c214d8f90a454
Mouthhan <50419632+Mouthhan@users.noreply.github.com>
2022-04-21 17:02:40 +0800

    Add mkdir

--- preprocess.py
+++ preprocess.py
@@ -3,39 +3,46 @@ import cv2
 import json
 import os
 
-name_dir = os.listdir('Train_Annotations')
-name_dir = [name[:-5] for name in name_dir]
 
 
-for name in name_dir[:10]:
-    image = cv2.imread(f'Train_Images/{name}.jpg')
+def preprocess():
+    name_dir = os.listdir('Train_Annotations')
+    name_dir = [name[:-5] for name in name_dir]
 
-    polys = []
-    with open(f'Train_Annotations/{name}.json') as f:
-        datas = json.load(f)
-        for data in datas['shapes']:
-            data = np.array(data['points'],dtype=np.int32)
-            polys.append(data)
-            print(np.mean(data, axis=0, dtype=np.int32))
+    os.mkdir('./Train_Mask',exist_ok = True)
+    os.mkdir('./Train_Masked_Images',exist_ok = True)
+    for name in name_dir[:10]:
+        image = cv2.imread(f'Train_Images/{name}.jpg')
 
-    polys = np.array(polys)
+        polys = []
+        with open(f'Train_Annotations/{name}.json') as f:
+            datas = json.load(f)
+            for data in datas['shapes']:
+                data = np.array(data['points'],dtype=np.int32)
+                polys.append(data)
+                print(np.mean(data, axis=0, dtype=np.int32))
 
-    im = np.zeros(image.shape[:2],dtype='uint8')
+        polys = np.array(polys)
 
-    for poly in polys:
-        poly = np.reshape(poly,(1,poly.shape[0],poly.shape[1]))
-        cv2.polylines(im, poly, 1, 255)
-        cv2.fillPoly(im,poly, 255)
+        im = np.zeros(image.shape[:2],dtype='uint8')
 
-    mask = im
-    # cv2.namedWindow("Mask",0)
-    # cv2.resizeWindow("Mask", 858, 471)
-    # cv2.imshow('Mask', mask)
-    masked = cv2.bitwise_and(image, image ,mask=mask)
-    cv2.imwrite(f'Train_Mask/{name}.jpg', mask)
-    cv2.imwrite(f'Train_Masked_Images/{name}.jpg', masked)
-    # cv2.namedWindow("Masked",0)
-    # cv2.resizeWindow("Masked", 858, 471)
-    # cv2.imshow('Masked', masked)
-    # cv2.waitKey(0)
-    # cv2.destroyAllWindows()
+        for poly in polys:
+            poly = np.reshape(poly,(1,poly.shape[0],poly.shape[1]))
+            cv2.polylines(im, poly, 1, 255)
+            cv2.fillPoly(im,poly, 255)
+
+        mask = im
+        # cv2.namedWindow("Mask",0)
+        # cv2.resizeWindow("Mask", 858, 471)
+        # cv2.imshow('Mask', mask)
+        masked = cv2.bitwise_and(image, image ,mask=mask)
+        cv2.imwrite(f'Train_Mask/{name}.jpg', mask)
+        cv2.imwrite(f'Train_Masked_Images/{name}.jpg', masked)
+        # cv2.namedWindow("Masked",0)
+        # cv2.resizeWindow("Masked", 858, 471)
+        # cv2.imshow('Masked', masked)
+        # cv2.waitKey(0)
+        # cv2.destroyAllWindows()
+
+if __name__=='__main__':
+    preprocess()
--- train.py
+++ train.py
@@ -10,124 +10,129 @@ from torchsummary import summary
 from mean_iou_evaluate import mean_iou_score, read_masks
 from model import UNet
 
-img_root = 'Train_Images/'
-json_root = 'Train_Annotations/'
-model_path = 'models/UNET_crop448.ckpt'
-name_dir = os.listdir('Train_Annotations')
-names = [name[:-5] for name in name_dir]
+def train():
+    os.mkdir('./models', exist_ok=True)
+    img_root = 'Train_Images/'
+    json_root = 'Train_Annotations/'
+    model_path = 'models/UNET_crop448.ckpt'
+    name_dir = os.listdir('Train_Annotations')
+    names = [name[:-5] for name in name_dir]
 
-train_idx = [idx for idx in range(len(names)) if idx % 10 != 0]
-valid_idx = [idx for idx in range(len(names)) if idx % 10 == 0]
+    train_idx = [idx for idx in range(len(names)) if idx % 10 != 0]
+    valid_idx = [idx for idx in range(len(names)) if idx % 10 == 0]
 
-all_dataset = STASDataset(json_root, img_root, names)
-train_set = Subset(all_dataset, train_idx)
-valid_set = Subset(all_dataset, valid_idx)
+    all_dataset = STASDataset(json_root, img_root, names)
+    train_set = Subset(all_dataset, train_idx)
+    valid_set = Subset(all_dataset, valid_idx)
 
-BATCH_SIZE = 2
-train_loader = DataLoader(train_set, batch_size = BATCH_SIZE, shuffle=True)
-valid_loader = DataLoader(valid_set, batch_size = BATCH_SIZE)
+    BATCH_SIZE = 2
+    train_loader = DataLoader(train_set, batch_size = BATCH_SIZE, shuffle=True)
+    valid_loader = DataLoader(valid_set, batch_size = BATCH_SIZE)
 
-device = 'cuda' if torch.cuda.is_available() else 'cpu'
-model = UNet(3, 2).to(device)
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    model = UNet(3, 2).to(device)
 
 
-# summary(model, (3, 224, 224), device="cuda")
+    # summary(model, (3, 224, 224), device="cuda")
 
-EPOCHS = 50
-lr = 3e-4
-decay = 5e-4
-accum_iter = 32
-early_stop = 50
-best_miou = 0.0
+    EPOCHS = 50
+    lr = 3e-4
+    decay = 5e-4
+    accum_iter = 32
+    early_stop = 50
+    best_miou = 0.0
 
-weight = [0.5, 2.5]
-class_weights = torch.FloatTensor(weight).to(device)
-# criterion = nn.CrossEntropyLoss(weight=class_weights)
-criterion = nn.CrossEntropyLoss()
-optimizer = torch.optim.AdamW(model.parameters(),lr=lr,weight_decay=decay)
-scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=300)
+    weight = [0.5, 2.5]
+    class_weights = torch.FloatTensor(weight).to(device)
+    # criterion = nn.CrossEntropyLoss(weight=class_weights)
+    criterion = nn.CrossEntropyLoss()
+    optimizer = torch.optim.AdamW(model.parameters(),lr=lr,weight_decay=decay)
+    scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=300)
 
-print('Start Training')
-for epoch in range(EPOCHS):
-    model.train()
-    train_loss = []
-    train_miou = 0
-    optimizer.zero_grad()
-    preds = []
-    masks = []
-    for batch_idx, batch in enumerate(tqdm(train_loader)):
-        with torch.set_grad_enabled(True):
-            imgs, labels = batch
-            imgs = imgs.to(device)
-            labels = labels.to(device)
-            logits = model(imgs)
-            
-            loss = criterion(logits,labels)
-            train_loss.append(loss.detach().item())
-            loss = loss / accum_iter
-            loss.backward()
-            
+    print('Start Training')
+    for epoch in range(EPOCHS):
+        model.train()
+        train_loss = []
+        train_miou = 0
+        optimizer.zero_grad()
+        preds = []
+        masks = []
+        for batch_idx, batch in enumerate(tqdm(train_loader)):
+            with torch.set_grad_enabled(True):
+                imgs, labels = batch
+                imgs = imgs.to(device)
+                labels = labels.to(device)
+                logits = model(imgs)
+                
+                loss = criterion(logits,labels)
+                train_loss.append(loss.detach().item())
+                loss = loss / accum_iter
+                loss.backward()
+                
 
-            preds += list(logits.cpu().argmax(dim=1).numpy())
-            masks += list(labels.cpu().numpy())
-            
+                preds += list(logits.cpu().argmax(dim=1).numpy())
+                masks += list(labels.cpu().numpy())
+                
 
-        ## Update weights
-            if ((batch_idx + 1) % accum_iter == 0) or (batch_idx + 1 == len(train_loader)):
-                optimizer.step()
-                scheduler.step()
-                optimizer.zero_grad()
-    preds = np.array(preds)
-    masks = np.array(masks)
-    # print(preds.shape)
-    # print(masks.shape)
-    train_loss = sum(train_loss) / len(train_loss)
-    train_miou = mean_iou_score(preds, masks)
+            ## Update weights
+                if ((batch_idx + 1) % accum_iter == 0) or (batch_idx + 1 == len(train_loader)):
+                    optimizer.step()
+                    scheduler.step()
+                    optimizer.zero_grad()
+        preds = np.array(preds)
+        masks = np.array(masks)
+        # print(preds.shape)
+        # print(masks.shape)
+        train_loss = sum(train_loss) / len(train_loss)
+        train_miou = mean_iou_score(preds, masks)
 
-    # Print the information.
-    print(f"[ Train | {epoch + 1:03d}/{EPOCHS:03d} ] Loss = {train_loss:.5f}, MIOU = {train_miou:.4f}")
+        # Print the information.
+        print(f"[ Train | {epoch + 1:03d}/{EPOCHS:03d} ] Loss = {train_loss:.5f}, MIOU = {train_miou:.4f}")
 
-    # ---------- Validation ----------
-    
-    valid_loss = []
-    valid_miou = 0
-    valid_preds = []
-    valid_masks = []
-    # Iterate the validation set by batches.
-    model.eval()
-    with torch.no_grad():
-        for idx, batch in enumerate(tqdm(valid_loader)):
-            imgs, labels = batch
-            imgs = imgs.to(device)
-            labels = labels.to(device)
-            logits = model(imgs)
+        # ---------- Validation ----------
+        
+        valid_loss = []
+        valid_miou = 0
+        valid_preds = []
+        valid_masks = []
+        # Iterate the validation set by batches.
+        model.eval()
+        with torch.no_grad():
+            for idx, batch in enumerate(tqdm(valid_loader)):
+                imgs, labels = batch
+                imgs = imgs.to(device)
+                labels = labels.to(device)
+                logits = model(imgs)
 
-            loss = criterion(logits, labels)
-            pred = logits.cpu().argmax(dim=1).numpy()
-            valid_preds += list(pred)
-            valid_masks += list(labels.cpu().numpy())
+                loss = criterion(logits, labels)
+                pred = logits.cpu().argmax(dim=1).numpy()
+                valid_preds += list(pred)
+                valid_masks += list(labels.cpu().numpy())
 
-            valid_loss.append(loss.item())
-            # valid_mious.append(miou)
-        # The average loss and accuracy for entire validation set is the average of the recorded values.
-    valid_preds = np.array(valid_preds)
-    valid_masks = np.array(valid_masks)
-    # print(valid_preds.shape)
-    # print(valid_masks.shape)
-    valid_loss = sum(valid_loss) / len(valid_loss)
-    valid_miou = mean_iou_score(valid_preds, valid_masks)
+                valid_loss.append(loss.item())
+                # valid_mious.append(miou)
+            # The average loss and accuracy for entire validation set is the average of the recorded values.
+        valid_preds = np.array(valid_preds)
+        valid_masks = np.array(valid_masks)
+        # print(valid_preds.shape)
+        # print(valid_masks.shape)
+        valid_loss = sum(valid_loss) / len(valid_loss)
+        valid_miou = mean_iou_score(valid_preds, valid_masks)
 
-    # Print the information.
-    print(f"[ Valid | {epoch + 1:03d}/{EPOCHS:03d} ] Loss = {valid_loss:.5f}, MIOU = {valid_miou:.4f}")
-    # Save Best Model
-    if valid_miou > best_miou:
-        torch.save(model.state_dict(),model_path)
-        print(f'Save Model With MIOU: {valid_miou:.4f}')
-        best_miou = valid_miou
-        stop_count = 0
-    else:
-        stop_count += 1
-        if stop_count > early_stop:
-            break
+        # Print the information.
+        print(f"[ Valid | {epoch + 1:03d}/{EPOCHS:03d} ] Loss = {valid_loss:.5f}, MIOU = {valid_miou:.4f}")
+        # Save Best Model
+        if valid_miou > best_miou:
+            torch.save(model.state_dict(),model_path)
+            print(f'Save Model With MIOU: {valid_miou:.4f}')
+            best_miou = valid_miou
+            stop_count = 0
+        else:
+            stop_count += 1
+            if stop_count > early_stop:
+                break
 
-print('End of Training')
+    print('End of Training')
+
+if __name__=='__main__':
+    train()
